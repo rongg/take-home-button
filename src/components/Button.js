af06eabba1ce091846a6896f57699f335269c493
GaydaR <ronald.gayda.13@gmail.com>
2020-03-26 11:45:32 -0700

    Implemented button layout and styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,21 @@ import * as React from "react";
 
 
 export default class Button extends React.Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
 
-	render() {
-		return (
-			<div className={'button'}>
-				<div className={'circle'}/>
-				Button
-				<div className={'circle'}/>
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className={'button'}>
+                <div className={'inner-border'}>
+                    <div className={'circle'}/>
+                    <div className={'text'}>BUTTON</div>
+                    <div className={'circle'}/>
+                </div>
+            </div>
+        );
+    }
 }
